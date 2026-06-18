827b089402f95cd19013e2654651d97931e7ce74
yasirimran4 <yasirimrandev@gmail.com>
2026-06-19 00:01:38 +0500

    Bug removed of celery

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -3,4 +3,7 @@ from .config import settings
 
 celery_app = Celery("meetlify" ,broker=settings.REDIS_URL,backend=settings.REDIS_URL)
 
-celery_app.autodiscover_tasks(["tasks"])
+celery_app.conf.imports = [
+    "tasks.email_task",
+    "tasks.reminder_task"
+]
