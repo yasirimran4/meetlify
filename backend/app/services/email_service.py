8ef25bafc51c38cfa2fb2d5b10e5835c9db2ac71
yasirimran4 <yasirimrandev@gmail.com>
2026-06-21 14:06:31 +0500

    Jinja Templates are integrated

--- backend/app/services/email_service.py
+++ backend/app/services/email_service.py
@@ -1,6 +1,24 @@
 from email.message import EmailMessage
 import aiosmtplib
 from core.config import settings
+from pathlib import Path
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+print(BASE_DIR)
+
+template_env = Environment(
+    loader=FileSystemLoader(BASE_DIR / "templates")
+)
+
+def render_template(template_name :str,context:dict):
+    template = template_env.get_template(template_name)
+    return template.render(**context)
+    
+
 class EmailService:
     async def send_email(self,email_to,subject:str,body:str):
         message = EmailMessage()
