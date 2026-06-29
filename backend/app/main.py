d9169856b50c15edd9d899abb4b959f0f32e46f6
yasirimran4 <yasirimrandev@gmail.com>
2026-06-29 14:38:49 +0500

    Cors Settings added

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,23 +4,29 @@ from routes.admin_routes import admin_router
 from routes.auth import auth_router
 from core.exception_handler import app_exception_handler
 from exceptions.base import AppException
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="Meetlify Event Management System",version='1.0.1')
-from core.redis import redis_client
+
 @app.get('/health')
 async def health():
     return {"status" : "Yes it's working" }
 
-@app.get('/api/v1/redis')
-async def redis_test():
-    await redis_client.set("Name","Yasir")
-    return {"name" : "Yasir" }
 
-
+# CORS Settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Including routes
 app.include_router(router=admin_router)
 app.include_router(router=event_router)
 app.include_router(router=auth_router)
-
 
 
 
